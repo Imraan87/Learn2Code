641114caac1feb1b73726705fa504c640e177af8
Imraan <imraan.ibrahim@Socionext.com>
2017-12-10 13:20:53 +0000

    testcode of calling python libraries from matlab

--- Matlab/Call PythonLibraries/Test1/TestScript.m
+++ Matlab/Call PythonLibraries/Test1/TestScript.m
@@ -1,22 +1,5 @@
-[version, executable, isloaded] = pyversion;
+addpath('..')
+common.PythonHandler();
 
 
-LogPlot     = py.logplot.LogPlot('./');
-diagnostics = py.diagnostics.Diagnostics();
-results = diagnostics.from_file('C:\\Users\\Imraan\\PycharmProjects\\crossLang\\corellian_74G.log');
-LogPlot.plot(results, './results')
-
-
-%%
 C = py.people.Customer('Python',30)
-C2 = Customer('Matlab',677)
-LogPlot = py.logplot.LogPlot('./')
-
-
-diagnostics = py.diagnostics.Diagnostics()
-results = diagnostics.from_file('C:\\Users\\Imraan\\PycharmProjects\\crossLang\\corellian_74G.log')
-
-LogPlot.dac_initialisation(results{'DACQ'}, 'DACQ', '.\')
-
-
-C.deposit(50)
